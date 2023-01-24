ca8d550491e4e49307956c32422cd90b23eece53
whoAmI-3 <aman.jham@flipkart.com>
2023-01-24 14:27:46 +0530

    Initial Commit

---
+++ DemoProject/src/com/flipkart/app/DemoClass.java
@@ -0,0 +1,10 @@
+package com.flipkart.app;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aman Hello");
+	}
+
+}
